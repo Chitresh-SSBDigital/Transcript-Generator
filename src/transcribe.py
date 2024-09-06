528868ffbf9340ff91ea2c74fef26843315ca7a7
Chitresh-SSBDigital <chitresh.gyanani@ssbi.in>
2024-09-06 10:01:32 +0530

    Update transcribe.py

--- src/transcribe.py
+++ src/transcribe.py
@@ -4,6 +4,13 @@ import warnings
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
 warnings.filterwarnings("ignore", category=FutureWarning, message=".*torch.load.*")
 
+# Size	  Parameters	English-only model	Multilingual model	Required VRAM	Relative speed
+# tiny	  39 M	        tiny.en	            tiny	            ~1 GB	        ~32x
+# base	  74 M	        base.en	            base	            ~1 GB	        ~16x
+# small	  244 M	        small.en	        small	            ~2 GB	        ~6x
+# medium  769 M	        medium.en	        medium	            ~5 GB	        ~2x
+# large	  1550 M	    N/A	                large	            ~10 GB	         1x
+
 def transcribe_audio(audio_file_path: str) -> str:
     """
     Transcribes an audio file into text using OpenAI's Whisper model.
